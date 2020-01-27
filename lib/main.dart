8ebd5b8412a96a75b5c320455e287640ef08ef5a
Benliam12 <s100live.ca@live.ca>
2020-01-27 10:56:51 -0500

    Starting ChatBot

--- lib/main.dart
+++ lib/main.dart
@@ -45,8 +45,80 @@ class _MyAppState extends  State<MyApp> {
         ),
 
         body: Container(
-          color: Colors.white,
+          color: Colors.transparent,
+          child: Wrap(
+            children: <Widget>[
+              Container(height: 10,),
+              Messages(),
+              Messages(reader: false, message: "Bonjour",),
+              Messages(),
+              Messages(),
+              Messages(message: "Salsssssssssssssssssssssssssssut",),
+              Messages(),
+              Messages(),
+              Messages(),
+              Messages(),
+              Messages(),
+              Messages(),
+              Messages(),
+              Messages(),
+            ],
+          ),
         ),
+      )
+    );
+  }
+}
+
+class Messages extends StatelessWidget
+{
+  final String message;
+  final bool reader;
+  
+  double readerReader = 20;
+  double senderReader = 0;
+  Color color = Colors.grey[300];
+  Color textColor = Colors.black;
+  Alignment alignment = Alignment.topLeft;
+
+  Messages({Key key, 
+    this.message = "A Simple Message",
+    this.reader = true,
+  }) : super(key:key);
+
+
+  @override
+  Widget build(BuildContext context)
+  {
+    if(reader)
+    {
+      this.senderReader = 20;
+      this.readerReader = 0;
+      this.color = Colors.blue;
+      this.textColor = Colors.white;
+      this.alignment = Alignment.topRight;
+    }
+
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.0, right:10.0, left: 10.0),
+      child: Align(
+        alignment: this.alignment,
+        child: FittedBox(
+          fit: BoxFit.fill, // otherwise the logo will be tiny
+          child: Container(
+              decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomLeft: Radius.circular(this.senderReader),
+                bottomRight: Radius.circular(this.readerReader)
+              ),
+              color: this.color
+            ),
+            padding: EdgeInsets.all(10.0),
+            child: Text(this.message, style:TextStyle(color: this.textColor)),
+          ),
+      )
       )
     );
   }
